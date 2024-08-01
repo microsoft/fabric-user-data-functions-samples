47339d577b7f353fbe782b6d5268813583e1fad4
Calvin Hsia <15060143+calvinhsia@users.noreply.github.com>
2024-08-01 12:50:02 -0700

    comments

--- DotNet/Samples.cs
+++ DotNet/Samples.cs
@@ -1,13 +1,12 @@
-/*
-Use an English language dictionary to get random words
-https://www.nuget.org/packages/DictionaryLib_Calvin_Hsia
+/* Description
+Use an English language dictionary to get random words  https://www.nuget.org/packages/DictionaryLib_Calvin_Hsia
 Contains 2 English Language Dictionaries: small (53,000 words) and large (172,000 words)
 Allows you to see if a string is a valid English word. Also can generate random words. Works on Windows, Android
 Sample Android Phone word game https://github.com/calvinhsia/WordScape
 */
 
-/*
-Usings: No new usings required
+/* How to use:
+Usings: No new 'usings' required
 PackageReference: Need to modify the HelloFabric.csproj to add this line:
  		<PackageReference Include="DictionaryLib_Calvin_Hsia" Version="1.0.7" />
 */
