4aa3af1602b4595e4ad819f0e04fc84c09427077
Calvin Hsia <15060143+calvinhsia@users.noreply.github.com>
2024-08-01 12:55:54 -0700

    jsonval

--- DotNet/Samples.cs
+++ DotNet/Samples.cs
@@ -24,3 +24,31 @@ PackageReference: Need to modify the HelloFabric.csproj to add this line:
             var result = Enumerable.Range(0, length.Value).Select(i => dict.RandomWord()).ToList(); // gets a list of random words
             return string.Join(" ", result);
         }
+
+
+/* Description 
+This sample validates a JSON string schema 
+*/
+
+[Function("ValidateJSONSchema")] 
+        public bool ValidateJSONSchema(string message) // Pass the JSON object as a string for message variable
+        {
+            _logger.LogInformation("Validating JSON Schema for message: {message}", message);
+
+            //define the JSON schema
+            JSchema schema = JSchema.Parse(@"{
+                'type': 'object',
+                'properties': {
+                    'name': {'type':'string'},
+                    'roles': {'type': 'array'}
+                }
+                }");
+                     
+                bool valid= false;
+                if (message != null)
+                {                
+                    JObject input = JObject.Parse(message);
+                    valid = input.IsValid(schema);
+                }
+                return valid;  
+        }
