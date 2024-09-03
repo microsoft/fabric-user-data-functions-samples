111b4784b76629cc429d83dace2524c25dc24a61
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-09-03 10:02:34 -0700

    Create readdatafromwarehouse.py

---
+++ PYTHON/readdatafromwarehouse.py
@@ -0,0 +1,28 @@
+# Example of using a FabricItemInput to query a Warehouse and then write the data to a csv in a Lakehouse
+# Uncomment and fill in the Warehouse alias and Lakehouse alias you would like to use
+@app.fabric_item_input(argName="myWarehouse", alias="<My Warehouse Alias>")
+@app.function("query_warehouse")
+def query_warehouse_and_write_to_csv(myWarehouse: fabric.functions.FabricSqlConnection) -> fabric.functions.UdfResponse:
+
+    whSqlConnection = myWarehouse.connect()
+    # Use connection to execute a query
+    # https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16#execute-a-query
+
+    cursor = whSqlConnection.cursor()
+    cursor.execute(f"SELECT * FROM (VALUES ('John Smith',  31) , ('Kayla Jones', 33)) AS Employee(EmpName, DepID);")
+
+    # Turn the rows into a json object
+    values = []
+    for row in rows:
+         item = {}
+        for prop, val in zip(columnNames, row):
+            if isinstance(val, (datetime, date)):
+                val = val.isoformat()
+            item[prop] = val
+        values.append(item)
+    
+    valJSON = json.dumps("values": values)
+    cursor.close()
+    whSqlConnection.close()
+
+    return fabric.functions.UdfResponse(valJSON)
