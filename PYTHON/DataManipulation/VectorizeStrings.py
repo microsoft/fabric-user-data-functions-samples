95db06d66240ead85678dffb25bfc4aad49ad471
marshall-lerner <66791488+marshall-lerner@users.noreply.github.com>
2024-09-24 15:47:07 -0700

    Change samples to stop returning UdfResponse and fix up formatting of some of the other samples (#9)
    * Update ReadDataFromWarehouse.py to remove UdfResponse
    
    * Update QueryWarehouseAndWriteToCsv.py to remove UdfResponse
    
    * Update VectorizeStrings.py to remove UdfResponse

--- PYTHON/DataManipulation/VectorizeStrings.py
+++ PYTHON/DataManipulation/VectorizeStrings.py
@@ -1,10 +1,12 @@
+# This sample vectorizes a string or text
 
-
-# This sample vectorizes a string or text 
-from sklearn.feature_extraction.text import CountVectorizer
+# Requirements.txt changes required:
+#     uncomment the line "scikit-learn"
+# Imports Statement changes required:
+#   from sklearn.feature_extraction.text import CountVectorizer
 
 @app.function("vectorize_string")
-def vectorize_string(text: str)-> fabric.functions.UdfResponse:
+def vectorize_string(text: str)-> str:
     """
     Vectorizes a string using CountVectorizer from sklearn.
 
@@ -21,6 +23,6 @@ def vectorize_string(text: str)-> fabric.functions.UdfResponse:
         featurenames= " ,".join(str(x) for x in vectorizer.get_feature_names_out())
         print("Vectorized text:\n", vectorized_text.toarray())
         print("Feature names:\n",vectorizer.get_feature_names_out())
-        return fabric.functions.UdfResponse("vectorized_text: " + vectors + "\nfeature_names: " + featurenames)
+        return "vectorized_text: " + vectors + "\nfeature_names: " + featurenames
     except Exception as e:
-        return fabric.functions.UdfResponse("An error occurred during vectorization:", e)
+        return "An error occurred during vectorization: " + str(e)
--- PYTHON/Warehouse/ReadDataFromWarehouse.py
+++ PYTHON/Warehouse/ReadDataFromWarehouse.py
@@ -1,8 +1,13 @@
 # Example of using a FabricItemInput to query a Warehouse and then write the data to a csv in a Lakehouse
 # Uncomment and fill in the Warehouse alias and Lakehouse alias you would like to use
+
+# Imports Statement changes required:
+#    import datetime
+#    import json
+
 @app.fabric_item_input(argName="myWarehouse", alias="<My Warehouse Alias>")
 @app.function("query_warehouse")
-def query_warehouse_and_write_to_csv(myWarehouse: fabric.functions.FabricSqlConnection) -> fabric.functions.UdfResponse:
+def query_warehouse(myWarehouse: fabric.functions.FabricSqlConnection) -> str:
 
     whSqlConnection = myWarehouse.connect()
     # Use connection to execute a query
@@ -10,19 +15,21 @@ def query_warehouse_and_write_to_csv(myWarehouse: fabric.functions.FabricSqlConn
 
     cursor = whSqlConnection.cursor()
     cursor.execute(f"SELECT * FROM (VALUES ('John Smith',  31) , ('Kayla Jones', 33)) AS Employee(EmpName, DepID);")
-
+    
+    rows = [x for x in cursor]
+    columnNames = [x[0] for x in cursor.description]
     # Turn the rows into a json object
     values = []
     for row in rows:
-         item = {}
+        item = {}
         for prop, val in zip(columnNames, row):
-            if isinstance(val, (datetime, date)):
+            if isinstance(val, (datetime.date, datetime.datetime)):
                 val = val.isoformat()
             item[prop] = val
         values.append(item)
     
-    valJSON = json.dumps("values": values)
+    valJSON = json.dumps({"values": values})
     cursor.close()
     whSqlConnection.close()
 
-    return fabric.functions.UdfResponse(valJSON)
+    return valJSON
