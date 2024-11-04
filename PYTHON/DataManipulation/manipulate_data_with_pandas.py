70c9dcd3015fb579b08c854edf1c4c52d3b0bacf
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-04 13:55:09 -0800

    Update manipulate_data_with_pandas.py

--- PYTHON/DataManipulation/manipulate_data_with_pandas.py
+++ PYTHON/DataManipulation/manipulate_data_with_pandas.py
@@ -5,17 +5,17 @@
 import pandas as pd 
 @app.function("manipulate_data")
 def manipulate_data(data: dict)-> str:
+    items= data["data"]
     # Convert the data dictionary to a DataFrame
-    df = pd.DataFrame(data)
-
-    # Perform basic data manipulation
-    # Example: Add a new column 'AgeGroup' based on the 'Age' column
+    df = pd.DataFrame(items)
+        # Perform basic data manipulation
+    # Example: Add a new column 'AgeGroup' based on the 'Age' column    
     df['AgeGroup'] = df['Age'].apply(lambda x: 'Adult' if x >= 18 else 'Minor')
-
+    
     # Example: Filter rows where 'Age' is greater than 30
-    df_filtered = df[df['Age'] > 30]
+    df_filtered = df[df["Age"] > 30]
 
     # Example: Group by 'AgeGroup' and calculate the mean age
-    df_grouped = df.groupby('AgeGroup')['Age'].mean().reset_index()
+    df_grouped = df.groupby("AgeGroup")["Age"].mean().reset_index()
     resultsJSON = json.dumps({"values": df_grouped})          
     return resultsJSON
