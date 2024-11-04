4dd710cc66458c0c189b6813e79024036d837ba1
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-04 15:16:16 -0800

    Update write_csv_file_in_lakehouse.py

--- PYTHON/Lakehouse/write_csv_file_in_lakehouse.py
+++ PYTHON/Lakehouse/write_csv_file_in_lakehouse.py
@@ -3,8 +3,20 @@
 
 
 import pandas as pd 
+
 @app.fabric_item_input(argName="mylakehouse", alias="<My Lakehouse alias>")
 @app.function("write_csv_file_from_lakehouse")
 def write_csv_file_in_lakehouse(mylakehouse: fabric.functions.FabricSqlConnection)-> str:
-             
+    data = [(1,"John Smith", 31), (2,"Kayla Jones", 33)]
+    connection = mylakehouse.connectToFiles()
+    csvFileName = "Employees" + str(round(datetime.datetime.now().timestamp())) + ".csv"
+    csvFile = connection.get_file_client(csvFileName)
+    # Convert the data to a DataFrame
+    df = pd.DataFrame(data, columns=['ID','EmpName', 'DepID'])
+    # Write the DataFrame to a CSV file
+    df.to_csv(csvFile, index=False)
+    # csvFile.upload_data('\n'.join(csvRows), overwrite=True)
+  
+    csvFile.close()
+    connection.close()
     return resultsJSON
