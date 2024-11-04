36c938969b31a2697f94199ba04f92ad42d6a604
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-04 13:07:26 -0800

    Update ReadFromAzureSQL.py

--- PYTHON/AzureSQLDB/ReadFromAzureSQL.py
+++ PYTHON/AzureSQLDB/ReadFromAzureSQL.py
@@ -1,11 +1,11 @@
 # This sample allows you to read data from Azure SQL database 
 # Complete these steps before testing this funtion 
 #   1. Select Manage connections to connect to Azure SQL database 
+#   2. Copy the Alias name and replace it in line 6 
 
-
-@app.connection("sqlDB")
+@app.connection("sqlDB",alias="<alias for azure sql database>")
 @app.function("read_from_azure_sql_db")
-def read_from_azure_sql_db()->str:
+def read_from_azure_sql_db(sqlDB: udf.FabricSqlConnection)->str:
     # Replace with the query you want to run
       query = "SELECT * FROM (VALUES ('John Smith', 31), ('Kayla Jones', 33)) AS Employee(EmpName, DepID);"
   
