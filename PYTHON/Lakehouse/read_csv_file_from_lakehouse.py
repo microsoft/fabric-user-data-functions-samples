9ddc2aede7952960e6bfd69fadd4a8034c61be5e
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-05 10:38:29 -0800

    Update read_csv_file_from_lakehouse.py

--- PYTHON/Lakehouse/read_csv_file_from_lakehouse.py
+++ PYTHON/Lakehouse/read_csv_file_from_lakehouse.py
@@ -1,9 +1,14 @@
-# This sample reads a CSV file from a lakehouse 
 
+# This sample writes a CSV file from a lakehouse  using pandas 
+# Complete these steps before testing this funtion 
+#   1. Select Manage connections to connect to Lakehouse. 
+#   2. Select Library Management and add pandas library 
 
 import pandas as pd 
+
+#Replace the alias "<My Lakehouse alias>" with your connection alias.
 @app.fabric_item_input(argName="mylakehouse", alias="<My Lakehouse alias>")
 @app.function("read_csv_file_from_lakehouse")
 def read_csv_file_from_lakehouse(mylakehouse: fabric.functions.FabricSqlConnection)-> str:
              
-    return resultsJSON
+    return f"File {csvFileName} was read from the Lakehouse."
