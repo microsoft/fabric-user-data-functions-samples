18471fdfd7701bb5165ac53050b93659475d3553
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-06 14:30:11 -0800

    Update and rename write_to_azure_sql_db.py to write_to_sql_db.py

--- PYTHON/SQLDB/write_to_sql_db.py
+++ PYTHON/SQLDB/write_to_sql_db.py
@@ -3,9 +3,9 @@
 #   1. Select Manage connections to connect to Azure SQL database 
 #   2. Copy the Alias name and replace it in line 6 
 
-@app.connection("sqlDB",alias="<alias for azure sql database>")
-@app.function("write_to_azure_sql_db")
-def write_to_azure_sql_db(sqlDB: udf.FabricSqlConnection):
+@app.connection("sqlDB",alias="<alias for sql database>")
+@app.function("write_to_sql_db")
+def write_to_sql_db(sqlDB: udf.FabricSqlConnection):
     # Replace with the data you want to insert
     data = [(1,"John Smith", 31), (2,"Kayla Jones", 33),(3,"Edward Harris", 33)]
 
