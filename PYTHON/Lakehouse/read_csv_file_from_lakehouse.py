578e71c8ea4037d43e5074e50bc8d7a412d2dd44
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-07 10:21:21 -0800

    Update read_csv_file_from_lakehouse.py

--- PYTHON/Lakehouse/read_csv_file_from_lakehouse.py
+++ PYTHON/Lakehouse/read_csv_file_from_lakehouse.py
@@ -9,7 +9,7 @@ import pandas as pd
 #Replace the alias "<My Lakehouse alias>" with your connection alias.
 @app.fabric_item_input(argName="mylakehouse", alias="<My Lakehouse alias>")
 @app.function("read_csv_from_lakehouse")
-def read_csv_from_lakehouse(mylakehouse: udf.FabricLakehouseClient, csvFileName: str) -> str:
+def read_csv_from_lakehouse(mylakehouse: fabric.functions.FabricLakehouseClient, csvFileName: str) -> str:
 
     # Connect to the Lakehouse
     connection = myLakehouse.connectToFiles()   
