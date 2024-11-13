e2fa83208648c1ce085384263f4b26036a6e06c6
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-12 21:17:22 -0800

    Update read_from_sql_db.py

--- PYTHON/SQLDB/read_from_sql_db.py
+++ PYTHON/SQLDB/read_from_sql_db.py
@@ -3,7 +3,7 @@
 #   1. Select Manage connections to connect to a Fabric SQL Database
 #   2. Copy the Alias name and replace it below inside the @app.fabric_item_input() decorator.
 
-@app.fabric_item_input("sqlDB", alias="<alias for sql database>")
+@app.fabric_item_input(argName="sqlDB", alias="<alias for sql database>")
 @app.function("read_from_sql_db")
 def read_from_sql_db(sqlDB: fabric.functions.FabricSqlConnection)->str:
     # Replace with the query you want to run
