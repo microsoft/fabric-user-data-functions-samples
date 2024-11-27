0a8bf03138d6f133df24e4b8190ac7fb7f5a90dd
Sunitha Muthukrishna <3684166+mksuni@users.noreply.github.com>
2024-11-26 20:23:20 -0800

    Update read_from_sql_db.py

--- PYTHON/SQLDB/read_from_sql_db.py
+++ PYTHON/SQLDB/read_from_sql_db.py
@@ -1,11 +1,11 @@
 # This sample allows you to read data from a Fabric SQL Database 
 # Complete these steps before testing this function 
 #   1. Select Manage connections to connect to a Fabric SQL Database
-#   2. Copy the Alias name and replace it below inside the @app.fabric_item_input() decorator.
+#   2. Copy the Alias name and replace it below inside the @udf.connection() decorator.
 
-@app.fabric_item_input(argName="sqlDB", alias="<alias for sql database>")
-@app.function("read_from_sql_db")
-def read_from_sql_db(sqlDB: fabric.functions.FabricSqlConnection)->str:
+@udf.connection(argName="sqlDB",alias="<alias for sql database>")
+@udf.function()
+def read_from_sql_db(sqlDB: fn.FabricSqlConnection)->str:
     # Replace with the query you want to run
     query = "SELECT * FROM (VALUES ('John Smith', 31), ('Kayla Jones', 33)) AS Employee(EmpName, DepID);"
 
